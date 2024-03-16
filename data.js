3d07042b445da54ae5e651c058c28943ca783fe3
Swaraj Anand <swrjnnd@gmail.com>
2024-03-16 23:06:27 +0530

    Addded files

---
+++ data.js
@@ -0,0 +1,343 @@
+let data = [
+  {
+    id: 8,
+    name: "Crispy Chicken Sandwich",
+    images: [
+      {
+        sm: "https://s7d1.scene7.com/is/image/mcdonalds/t-mcdonalds-crispy-chicken-sandwich:1-4-product-tile-desktop",
+      },
+      {
+        lg: "https://s7d1.scene7.com/is/image/mcdonalds/t-mcdonalds-crispy-chicken-sandwich:product-header-mobile?wid=768&hei=443&dpr=off",
+      },
+    ],
+    desc: "McDonald's Crispy Chicken Sandwich is a southern style fried chicken sandwich that's crispy, juicy and tender perfection. It’s topped with crinkle-cut pickles and served on a toasted, buttered potato roll. The Crispy Chicken sandwich has 470 calories.",
+    ingredients: [
+      {
+        id: 1,
+        name: "Crispy Chicken Fillet",
+        img: "https://s7d1.scene7.com/is/image/mcdonalds/t-crispy-chicken-fillet",
+      },
+      {
+        id: 2,
+        name: "Potato Roll",
+        img: "https://s7d1.scene7.com/is/image/mcdonalds/t-potato-roll",
+      },
+      {
+        id: 3,
+        name: "Crinkle Cut Pickle",
+        img: "https://s7d1.scene7.com/is/image/mcdonalds/t-crinkle-cut-pickle",
+      },
+      {
+        id: 4,
+        name: "Salted Butter",
+        img: "https://s7d1.scene7.com/is/image/mcdonalds/butter_salted",
+      },
+    ],
+    price: 8,
+    veg: false,
+  },
+  {
+    id: 9,
+    name: "Deluxe Crispy Chicken Sandwich",
+    images: [
+      {
+        sm: "https://s7d1.scene7.com/is/image/mcdonalds/t-mcdonalds-deluxe-crispy-chicken-sandwich:1-4-product-tile-desktop",
+      },
+      {
+        lg: "https://s7d1.scene7.com/is/image/mcdonalds/t-mcdonalds-deluxe-crispy-chicken-sandwich:product-header-mobile?wid=768&hei=443&dpr=off",
+      },
+    ],
+    desc: "Get a little extra with toppings. Go deluxe with shredded lettuce, Roma tomatoes and mayo to take crispy, juicy and tender to the next level. The Deluxe Crispy Chicken sandwich has 530 calories.",
+    ingredients: [
+      {
+        id: 1,
+        name: "Crispy Chicken Fillet",
+        img: "https://s7d1.scene7.com/is/image/mcdonalds/t-crispy-chicken-fillet",
+      },
+      {
+        id: 2,
+        name: "Potato Roll",
+        img: "https://s7d1.scene7.com/is/image/mcdonalds/t-potato-roll",
+      },
+      {
+        id: 3,
+        name: "Roma Tomato",
+        img: "https://s7d1.scene7.com/is/image/mcdonalds/Ingredients_Tomato_180x180",
+      },
+      {
+        id: 4,
+        name: "Shredded Lettuce",
+        img: "https://s7d1.scene7.com/is/image/mcdonalds/shredded_lettuce",
+      },
+      {
+        id: 5,
+        name: "Mayonnaise",
+        img: "https://s7d1.scene7.com/is/image/mcdonalds/mayonnaise",
+      },
+    ],
+    price: 10,
+    veg: false,
+  },
+  {
+    id: 10,
+    name: "Spicy Crispy Chicken Sandwich",
+    images: [
+      {
+        sm: "https://s7d1.scene7.com/is/image/mcdonalds/t-mcdonalds-spicy-crispy-chicken-sandwich:1-4-product-tile-desktop",
+      },
+      {
+        lg: "https://s7d1.scene7.com/is/image/mcdonalds/t-mcdonalds-spicy-crispy-chicken-sandwich:product-header-mobile?product-header-desktop&wid=768&hei=443&dpr=off",
+      },
+    ],
+    desc: "With our Spicy Pepper Sauce topping the southern style fried chicken fillet on a toasted potato roll, this sandwich was made for those who like it crispy, juicy, tender and hot. The Spicy Crispy Chicken sandwich has 530 calories.",
+    ingredients: [
+      {
+        id: 1,
+        name: "Crispy Chicken Fillet",
+        img: "https://s7d1.scene7.com/is/image/mcdonalds/t-crispy-chicken-fillet",
+      },
+      {
+        id: 2,
+        name: "Potato Roll",
+        img: "https://s7d1.scene7.com/is/image/mcdonalds/t-potato-roll",
+      },
+      {
+        id: 3,
+        name: "Spicy Pepper Sauce",
+        img: "https://s7d1.scene7.com/is/image/mcdonalds/t-original-spicy-sauce",
+      },
+      {
+        id: 4,
+        name: "Crinkle Cut Pickle",
+        img: "https://s7d1.scene7.com/is/image/mcdonalds/t-crinkle-cut-pickle",
+      },
+    ],
+    price: 9.6,
+    veg: false,
+  },
+  {
+    id: 11,
+    name: "Spicy Deluxe Crispy Chicken Sandwich",
+    images: [
+      {
+        sm: "https://s7d1.scene7.com/is/image/mcdonalds/t-mcdonalds-spicy-deluxe-crispy-chicken-sandwich:1-4-product-tile-desktop",
+      },
+      {
+        lg: "https://s7d1.scene7.com/is/image/mcdonalds/t-mcdonalds-spicy-deluxe-crispy-chicken-sandwich:product-header-mobile?wid=768&hei=441&dpr=off",
+      },
+    ],
+    desc: "The Spicy Deluxe Crispy Chicken is big on everything, including heat. Our southern-style fried chicken fillet on a potato roll, topped with shredded lettuce, Roma tomatoes and Spicy Pepper Sauce kicks crispy, juicy and tender up to the highest level. The Spicy Deluxe Crispy Chicken sandwich has 540 calories.",
+    ingredients: [
+      {
+        id: 1,
+        name: "Crispy Chicken Fillet",
+        img: "https://s7d1.scene7.com/is/image/mcdonalds/t-crispy-chicken-fillet",
+      },
+      {
+        id: 2,
+        name: "Potato Roll",
+        img: "https://s7d1.scene7.com/is/image/mcdonalds/t-potato-roll",
+      },
+      {
+        id: 3,
+        name: "Roma Tomato",
+        img: "https://s7d1.scene7.com/is/image/mcdonalds/Ingredients_Tomato_180x180",
+      },
+      {
+        id: 4,
+        name: "Spicy Pepper Sauce",
+        img: "https://s7d1.scene7.com/is/image/mcdonalds/t-original-spicy-sauce",
+      },
+      {
+        id: 5,
+        name: "Shredded Lettuce",
+        img: "https://s7d1.scene7.com/is/image/mcdonalds/shredded_lettuce",
+      },
+    ],
+    price: 15,
+    veg: false,
+  },
+  {
+    id: 12,
+    name: "Filet-O-Fish®",
+    images: [
+      {
+        sm: "https://s7d1.scene7.com/is/image/mcdonalds/t-mcdonalds-Filet-O-Fish-1:1-4-product-tile-desktop",
+      },
+      {
+        lg: "https://s7d1.scene7.com/is/image/mcdonalds/t-mcdonalds-Filet-O-Fish-1:product-header-mobile?wid=768&hei=443&dpr=off",
+      },
+    ],
+    desc: "Dive into our wild-caught Filet-O-Fish®! This McDonald’s fish sandwich has fish sourced from sustainably managed fisheries, on melty American cheese and topped with creamy McDonald’s tartar sauce, all served on a soft, steamed bun. There are 390 calories in McDonald's Filet-O-Fish.",
+    ingredients: [
+      {
+        id: 1,
+        name: "Fish Filet Patty",
+        img: "https://s7d1.scene7.com/is/image/mcdonalds/fish",
+      },
+      {
+        id: 2,
+        name: "Regular Bun",
+        img: "https://s7d1.scene7.com/is/image/mcdonalds/regular_bun",
+      },
+      {
+        id: 3,
+        name: "Tartar Sauce",
+        img: "https://s7d1.scene7.com/is/image/mcdonalds/default_logo",
+      },
+      {
+        id: 4,
+        name: "Pasteurized Process American Cheese Half Slice",
+        img: "https://s7d1.scene7.com/is/image/mcdonalds/american_cheese_half",
+      },
+      {
+        id: 5,
+        name: "Shredded Lettuce",
+        img: "https://s7d1.scene7.com/is/image/mcdonalds/shredded_lettuce",
+      },
+    ],
+    price: 7,
+    veg: false,
+  },
+  {
+    id: 13,
+    name: "McChicken®",
+    images: [
+      {
+        sm: "https://s7d1.scene7.com/is/image/mcdonalds/t-mcdonalds-McChicken-1:1-4-product-tile-desktop",
+      },
+      {
+        lg: "https://s7d1.scene7.com/is/image/mcdonalds/t-mcdonalds-McChicken-1:product-header-mobile?wid=768&hei=443&dpr=off",
+      },
+    ],
+    desc: "It’s a classic for a reason. Savor the satisfying crunch of our juicy chicken patty, topped with shredded lettuce and just the right amount of creamy mayonnaise, all served on a perfectly toasted bun.* The McChicken® has 400 calories.",
+    ingredients: [
+      {
+        id: 1,
+        name: "McChicken Patty",
+        img: "https://s7d1.scene7.com/is/image/mcdonalds/mcchicken",
+      },
+      {
+        id: 2,
+        name: "Regular Bun",
+        img: "https://s7d1.scene7.com/is/image/mcdonalds/regular_bun",
+      },
+      {
+        id: 3,
+        name: "Shredded Lettuce",
+        img: "https://s7d1.scene7.com/is/image/mcdonalds/shredded_lettuce",
+      },
+      {
+        id: 4,
+        name: "Mayonnaisee",
+        img: "https://s7d1.scene7.com/is/image/mcdonalds/mayonnaisef",
+      },
+    ],
+    price: 8,
+    veg: false,
+  },
+  {
+    id: 14,
+    name: "Big Mac®",
+    images: [
+      {
+        sm: "https://s7d1.scene7.com/is/image/mcdonalds/t-mcdonalds-Big-Mac-1:1-4-product-tile-desktop",
+      },
+      {
+        lg: "https://s7d1.scene7.com/is/image/mcdonalds/t-mcdonalds-Big-Mac-1:product-header-mobile?wid=768&hei=441&dpr=off",
+      },
+    ],
+    desc: "Ever wondered what's on a Big Mac®? The McDonald's Big Mac® is a 100% beef burger with a taste like no other. The mouthwatering perfection starts with two 100% pure all beef patties and Big Mac® sauce sandwiched between a sesame seed bun. It’s topped off with pickles, crisp shredded lettuce, finely chopped onion, and a slice of American cheese. It contains no artificial flavors, preservatives, or added colors from artificial sources. Our pickle contains an artificial preservative, so skip it if you like. There are 550 calories in a Big Mac® from McDonald's. Pair it with any of our beverages or grab a Big Mac® Combo Meal with our World Famous Fries® and Coca-Cola® or any of your favorite fountain drinks.",
+    ingredients: [
+      {
+        id: 1,
+        name: "Big Mac Bun",
+        img: "https://s7d1.scene7.com/is/image/mcdonalds/big_mac_bun",
+      },
+      {
+        id: 2,
+        name: "100% Beef Patty",
+        img: "https://s7d1.scene7.com/is/image/mcdonalds/10_1_patty",
+      },
+      {
+        id: 3,
+        name: "Shredded Lettuce",
+        img: "https://s7d1.scene7.com/is/image/mcdonalds/shredded_lettuce",
+      },
+      {
+        id: 4,
+        name: "Big Mac Sauce",
+        img: "https://s7d1.scene7.com/is/image/mcdonalds/big_mac_sauce",
+      },
+      {
+        id: 5,
+        name: "Pasteurized Process American Cheese",
+        img: "https://s7d1.scene7.com/is/image/mcdonalds/ingredient_american_cheese_180x180",
+      },
+      {
+        id: 6,
+        name: "Pickle Slices",
+        img: "https://s7d1.scene7.com/is/image/mcdonalds/pickles",
+      },
+      {
+        id: 7,
+        name: "Onions",
+        img: "https://s7d1.scene7.com/is/image/mcdonalds/reconstituted_onions",
+      },
+    ],
+    price: 10.45,
+    veg: false,
+  },
+  {
+    id: 15,
+    name: "Quarter Pounder®* with Cheese",
+    images: [
+      {
+        sm: "https://s7d1.scene7.com/is/image/mcdonalds/t-mcdonalds-Quarter-Pounder-with-Cheese-1:1-4-product-tile-desktop",
+      },
+      {
+        lg: "https://s7d1.scene7.com/is/image/mcdonalds/t-mcdonalds-Quarter-Pounder-with-Cheese-1:product-header-mobile?wid=768&hei=443&dpr=off",
+      },
+    ],
+    desc: "Each Quarter Pounder® with Cheese burger features a ¼ lb.* of 100% fresh beef that’s hot, deliciously juicy and cooked when you order. It’s seasoned with just a pinch of salt and pepper, sizzled on a flat iron grill, then topped with slivered onions, tangy pickles and two slices of melty American cheese on a sesame seed bun. Our QPC® contains no artificial flavors, preservatives or added colors from artificial sources. **Our pickle contains an artificial preservative, so skip it if you like. A Quarter Pounder® with Cheese has 520 Calories. ",
+    ingredients: [
+      {
+        id: 1,
+        name: "Quarter Pound 100% Beef Patty",
+        img: "https://s7d1.scene7.com/is/image/mcdonalds/default_logo",
+      },
+      {
+        id: 2,
+        name: "Quarter Pound Bun",
+        img: "https://s7d1.scene7.com/is/image/mcdonalds/quarter_pounder_bun",
+      },
+      {
+        id: 3,
+        name: "Pasteurized Process American Cheese",
+        img: "https://s7d1.scene7.com/is/image/mcdonalds/ingredient_american_cheese_180x180",
+      },
+      {
+        id: 4,
+        name: "Ketchup",
+        img: "https://s7d1.scene7.com/is/image/mcdonalds/ketchup",
+      },
+      {
+        id: 5,
+        name: "Pickle Slices",
+        img: "https://s7d1.scene7.com/is/image/mcdonalds/pickles",
+      },
+      {
+        id: 6,
+        name: "Onions",
+        img: "https://s7d1.scene7.com/is/image/mcdonalds/slivered_onions",
+      },
+      {
+        id: 7,
+        name: "Mustard",
+        img: "https://s7d1.scene7.com/is/image/mcdonalds/mustard",
+      },
+    ],
+    price: 6,
+    veg: false,
+  },
+];
